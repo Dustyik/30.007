04993acbe7561d5e119f244e854b1a76ecdc2419
Tan Chia Yik <Dustyik@MacBook-Air-8.local>
2020-03-24 01:12:33 +0800

    fifth commit

--- src/Components/picturedisplay.js
+++ src/Components/picturedisplay.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { photos } from "./photos"
 import Gallery from "./Gallery"
 import firebase from "./firebase/firebase.js"
 import { CommunicationPresentToAll } from "material-ui/svg-icons";
@@ -9,20 +8,19 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      photosurl: [],
+      pathurllength: null,
+      photosobject: {}
     }
     this.getPics = this.getPics.bind(this);
+    this.randint = this.randint.bind(this);
   }
 
-  async componentDidMount(){
-    let collection = await this.getPics(this.props.location.state.id);
-    this.setState({
-      photos: collection
-    })
-
+  async componentDidMount() {
+    await this.getPics(this.props.location.state.id)
   }
 
   async getPics(deets) {
-    console.log("get pics", deets)
     const storage = firebase.storage();
     var storageRef = storage.ref(deets)
     var listRef = storageRef.child('images');
@@ -42,36 +40,55 @@ class App extends React.Component {
       pathurl.push(path)
     }
 
-    console.log(pathurl)
+    this.setState({
+      pathurllength: pathurl.length
+    })
+
     var downloadurl = []
+    let obj = []
     for (var i = 0; i < pathurl.length; i++) {
       var starsRef = storageRef.child(pathurl[i]);
-      console.log(starsRef)
-      downloadurl.push(starsRef.getDownloadURL())
-    }
+      downloadurl.push(starsRef.getDownloadURL().then((url) => {
+        // downloadurl.add(url)
+        this.state.photosurl.push(url)
+        this.setState(this.state)
 
-    console.log(downloadurl, "downloadurl")
+        let emptyobj = {}
+        console.log("checking src", this.state.photosurl[i])
+        emptyobj.src = url
+        emptyobj.width = 3
+        emptyobj.height = 2
+        obj.push(emptyobj)
 
-    var photose = []
-    for (var i = 0; i < downloadurl.length; i++) {
-      let bleg = {
-        src: await downloadurl[i],
-        width: 1,
-        height: 1
+        this.setState({
+          photosobject: obj
+        })
+        console.log("createphotosobj", obj)
       }
-      photose.push(bleg)
+      ))
     }
-    console.log(photose)
 
-    return photose
+    // this.createphotosobject();
   }
 
+  randint(int) {
+    return Math.floor(Math.random() * int) + 1; //returns rand int between 1 to (int + 1)
+  }
 
   render() {
+    if(this.state.photosobject.length == this.state.pathurllength && this.state.photosobject.length > 0){
+      var exist = true
+    }
+    console.log(exist, this.state)
     return (
       <div>
-        <Gallery photos={photos}>
-        </Gallery>
+        <div>
+          {exist ? (
+            <Gallery photos={this.state.photosobject} />
+          ) : (
+              <div />
+            )}
+        </div>
       </div>
 
     );
